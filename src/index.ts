99c5386118e7108c0881fd38266c67731b439f0e
Md Zaid Quaraishi <mzq7080@gmail.com>
2025-07-14 21:21:11 +0530

    basic setup for a server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world!!")
+import http from 'http';
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.statusMessage = 'HAHA WORLD!!!';
+	res.end('<h1>Hello World!!</h1>');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
